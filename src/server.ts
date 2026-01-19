846abbcbda85c0bd0f004c5fcac7da616aa40f26
Raj Narayan <rajnarayankumar139@gmail.com>
2026-01-19 06:16:16 +0000

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { env } from "./config/env";
+
+const app = express();
+const port = env.port;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Taskflow saas API running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
